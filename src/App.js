70e592a06ef1d7ff46ca1a0d7e9286b87200b1fc
Juan Jose Bernal Villamarin <juanjosebernalvillamarin@gmail.com>
2024-02-09 14:29:36 -0500

    Resolving warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Collapse } from './components/Collapse';
 import { Timeline } from './components/Timeline';
@@ -7,11 +6,11 @@ import UglyCard from './components/UglyCard';
 function App() {
   return (
     <>
-    <Collapse />
-     <div style={{padding: '40px'}}>
-      <Timeline />
-    </div>
-    <UglyCard />
+      <Collapse />
+      <div style={{ padding: '40px' }}>
+        <Timeline />
+      </div>
+      <UglyCard />
     </>
   );
 }
--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -5,11 +5,6 @@ import { Label } from './Label';
 
 export const Collapse = () => {
 
-  const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
   const items = [
     {
       key: '1',
